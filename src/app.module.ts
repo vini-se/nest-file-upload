568709b0a4327c05bf8436fc2c5aaf43668f3599
Vinicius <vinicius.dla2106@gmail.com>
2025-05-19 18:35:46 -0300

    feat: configure prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UploadModule } from './upload/upload.module';
+import { UploadModule } from './app/modules/upload/upload.module';
+import { PrismaModule } from './infra/database/client/prisma.module';
 
 @Module({
-  imports: [UploadModule],
+  imports: [UploadModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/infra/database/client/prisma.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from 'generated/prisma';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super();
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
